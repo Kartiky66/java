3929b9926998dfbaf3f115ddfc0dc5738ecc4aa4
Kartik <118624981+Kartiky66@users.noreply.github.com>
2023-04-30 11:14:46 +0530

    hackerrank4

---
+++ hackerrank4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class hackerrank4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        String s = sc.next();
+        double d = sc.nextDouble();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
